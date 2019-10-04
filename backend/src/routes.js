903acf4bb8eedd726d560df24c51358fd13655bb
Filipe Marques <filipi.marques@gmail.com>
2019-10-04 14:34:22 -0300

    Finalizando a parte inicial do backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,9 @@ const multer = require('multer')
 
 const uploadCongig = require('./config/upload')
 const SesseionController = require('./controllers/SessionController')
-const SpotController = require('.//controllers/SpotCOntroller')
+const SpotController = require('./controllers/SpotController')
+const Dashboardcontroller = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingsController')
 
 
 
@@ -14,6 +16,10 @@ const upload = multer(uploadCongig)
 routes.post('/sessions', SesseionController.store)
 routes.get('/spots', SpotController.index)
 routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+
+routes.get('/dashboard', Dashboardcontroller.show)
 //Dentro da rota eu vou chamar o meu controller e o store que foi o método que fiz
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes
